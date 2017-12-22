49ae1ec43956c8033fbfde3a75fc9d3221bc019e
黄翔 <707405626@qq.com>
2017-12-22 14:42:12 +0800

    travis ci 第一次尝试

---
+++ travis.sh
@@ -0,0 +1,24 @@
+
+PURE_TAG=`git status | grep 'Not currently on any branch'`
+CUR_SHA=`git rev-parse HEAD`
+CUR_TAG=`git tag --points-at $CUR_SHA`
+
+echo ''
+echo git status: $PURE_TAG
+echo git tag: $CUR_TAG
+
+if [ $CUR_TAG ] && [ "$PURE_TAG"x != ""x ]; then
+
+echo ''
+echo '[is a tag] start packing'
+
+npm install
+
+npm start
+
+else
+
+echo ''
+echo '[not a tag] exit packing.'
+
+fi
